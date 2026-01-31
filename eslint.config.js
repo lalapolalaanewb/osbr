ae378a64077b59983f3d3edc692fc5782b07af92
Fathi Noor <mohdfathi_mhdnoor@yahoo.com>
2026-01-31 13:43:40 +0800

    fix: put no-unused-vars as warn

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,16 @@ export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
     ignores: ["dist", "node_modules", ".next"],
   },
 ];
